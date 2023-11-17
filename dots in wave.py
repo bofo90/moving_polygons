ed5aedc0d5b9e02e91cc6379215f9cc6d5724dc2
Agustin Iniguez <agustin.rabago@xomnia.com>
2023-11-17 15:16:59 +0100

    last changes

--- dots in wave.py
+++ dots in wave.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 from grid_classes import Lattice as Lat
 from plotting import Figure_Lattice as Fig
 
-tot_num_poly = 40
+tot_num_poly = 500
 angle = 35
 # approx number of wavelengths in the direction
 fr = 1.3
 
-grid = Lat(tot_num_poly, "tri", 4, 3, angle, fr)
+grid = Lat(tot_num_poly, "hex", 4, 3, angle, fr)
 fig = Fig()
 
 # fig.create_points(grid, slider=True)
 
-fig.create_poly(grid, t=0.25, slider=True)
+fig.create_poly(grid, t=0.25)
 
 plt.show()
